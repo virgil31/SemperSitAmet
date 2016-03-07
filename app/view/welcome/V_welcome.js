d877bfc827124d11c17c8b13b1a234dd15db2021
Luca Cerini <lucacerini@hotmail.it>
2016-03-07 16:22:02 +0100

    Immagine di arduino in percentuale nella welcome

--- app/view/welcome/V_welcome.js
+++ app/view/welcome/V_welcome.js
@@ -54,12 +54,12 @@ Ext.define('SemperSitAmet.view.welcome.V_welcome', {
                                             {
                                                 xtype: 'image',
                                                 src: "resources/images/connect_arduino.png",
-                                                width: 320,
+                                                width: "100%",//320,
                                                 height: 150,
                                                 margin: '10 0 0 0'
                                             },
                                             {
-                                                html: "<div style='text-align: center;'>Arduino viene distribuito con l'indirizzo predefinito 192.168.1.220. Assicurarsi quindi di trovarsi sulla sua stessa rete e che tale indirizzo non sia già occupato da un altro dispositivo.<br><br><br>Collegare Arduino alla rete adsl ed elettrica. Usare quindi il pulsante di seguito per effettuare un test veloce.</div>",
+                                                html: "<div style='text-align: center;'>Arduino viene distribuito con l'indirizzo predefinito 192.168.1.220. Assicurarsi quindi di trovarsi sulla sua stessa rete e che tale indirizzo non sia già occupato da un altro dispositivo.<br><br>Collegare Arduino alla rete adsl ed elettrica. Usare quindi il pulsante di seguito per effettuare un test veloce.</div>",
                                                 margin: '20 0 0 0'
                                             },
                                             {                                                
